fe1671d7867eb8286c261512056e255b1c1488b9
WilliamH100 <83245147+WilliamH100@users.noreply.github.com>
2021-04-26 20:41:00 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,13 @@
 input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
     soundExpression.hello.play()
     basic.showString("hello")
 })
 input.onButtonPressed(Button.B, function () {
     music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showIcon(IconNames.Heart)
 })
 music.setBuiltInSpeakerEnabled(true)
 basic.showIcon(IconNames.Happy)
